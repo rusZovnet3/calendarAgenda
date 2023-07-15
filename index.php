e933cbda20d5c5045ae01cedd7928dbac07309e1
rusZovnet3 <cferls_ru@hotmail.com>
2023-07-15 13:27:41 -0400

    Calendario, capturar el dato la fecha -- v1.2

--- index.php
+++ index.php
@@ -17,9 +17,55 @@
 <body>
 
 	<div class="container">
+
 		<div class="col-md-8 offset-md-2">
+
 			<div id="calendar"></div>
+
 		</div>
+
+		<!-- Button trigger modal -->
+	    <button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#modalEvento">
+	      Launch
+	    </button>
+
+		<!-- Modal -->
+		<div class="modal fade" id="modalEvento" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+
+			<div class="modal-dialog" role="document">
+
+				<div class="modal-content">
+
+					<div class="modal-header">
+
+						<h5 class="modal-title">Datos del evento </h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+					</div>
+
+					<div class="modal-body">
+
+						<div class="container-fluid">
+							Sin cargos
+						</div>
+
+					</div>
+
+					<div class="modal-footer">
+
+						<button type="button" class="btn btn-danger" id="btnBorrar" data-bs-dismiss="modal">Borrar</button>
+						<button type="button" id="btnGuardar" class="btn btn-primary">Guardar</button>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+
+
 	</div>
 
 
@@ -34,6 +80,10 @@
 					left: 'prev,next today',
 					center: 'title',
 					right: 'dayGridMonth,timeGridWeek,timeGridDay'  // mes,dia,semana === por tiempo
+				},
+				// captura la fecha, al hacer click el dia del mes
+				dateClick:function(informacion){
+					alert("Presionaste la fecha: " + informacion.dateStr);
 				}
 			});
 			calendar.render();
